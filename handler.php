827060fa30f6a64962afb2d2a7fbe4d02a8bfff5
Desiree <desireesanchezdev@gmail.com>
2024-02-01 13:04:03 +0100

    Added file that handles form data and Implemented creation and listing methods

---
+++ handler.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Controllers\TodoListController;
+use App\Models\TodoListModel;
+
+require_once 'vendor/autoload.php';
+include_once "./config/server.php";
+
+$tasksModel = new TodoListModel();
+$todoListController = new TodoListController($tasksModel);
+
+$tasksModel = $todoListController->index();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['title']) && (isset($_POST['descripcion']))) {
+        $data = [
+            'title' => $_POST['title'],
+            'descripcion' => $_POST['descripcion'],
+        ];
+        $result = $todoListController->create($data);
+        echo $result;
+    } elseif (isset($_POST['update'])) {
+        $id = $_POST['id'];
+        $result = $todoListController->update($id, $data);
+        echo $result;
+    } elseif (isset($_POST['delete'])) {
+        $id = $_POST['id'];
+    } else {
+        echo "Acción no reconocida";
+    }
+    header("location: index.php");
+}
