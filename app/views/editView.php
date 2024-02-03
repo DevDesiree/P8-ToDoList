dd1e776c5997e01b4dd0c761d51606621b055b91
Desiree <desireesanchezdev@gmail.com>
2024-02-03 19:34:44 +0100

    Added inc views on editView.php and edit styles Bootstrap

--- app/views/editView.php
+++ app/views/editView.php
@@ -1,18 +1,24 @@
+<?php
+$editedTask = $todoListController->edit($id);
+include "./app/views/inc/header.php";
+?>
+<main>
+    <div class="container-sm">
+        <form method='POST' action='./handler.php'>
+            <input type='hidden' name='id' value='<?= $editedTask['id'] ?>'>
+            <div class="form-group">
+                <label for="titleEdit">Titulo</label>
+                <input type="text" class="form-control" id="titleEdit" name="titleEdit" value="<?= $editedTask['title'] ?>">
+            </div>
+            <div class="form-group">
+                <label for="descripcionEdit">Descripcion</label>
+                <textarea class="form-control" id="descripcionEdit" name="descripcionEdit" rows="3"><?= $editedTask['descripcion'] ?></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary" name="update">Actualizar</button>
+        </form>
+    </div>
+</main>
 
 <?php
-
-$editedTask = $todoListController->edit($id);
+include "./app/views/inc/footer.php";
 ?>
-
-<form method='POST' action='./handler.php'>
-    <input type='hidden' name='id' value='<?= $editedTask['id'] ?>'>
-    <div class="form-group">
-        <label for="title">Titulo</label>
-        <input type="text" class="form-control" id="titleEdit" name="titleEdit" value="<?= $editedTask['title'] ?>">
-    </div>
-    <div class="form-group">
-        <label for="description">Descripcion</label>
-        <textarea class="form-control" id="descripcionEdit" name="descripcionEdit" rows="3"><?= $editedTask['descripcion'] ?></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary" name="update">Actualizar</button>
-</form>
