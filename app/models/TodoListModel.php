e0916183731f4b54467f746f488331d377601307
Desiree <desireesanchezdev@gmail.com>
2024-02-04 21:21:39 +0100

    Reestructured ConnectionDB and edit route on Model

--- app/models/TodoListModel.php
+++ app/models/TodoListModel.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use Database\ConnectionDB;
 
 use Exception;
 
--- database/ConnectionDB.php
+++ database/ConnectionDB.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace Database;
 
 include_once "./config/server.php";
 
